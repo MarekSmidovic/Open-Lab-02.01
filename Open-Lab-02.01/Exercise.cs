531c893aa85d2242d8d770cc8a69d99fc8a34d5d
smidovic <msmidovic@gmail.com>
2021-11-09 09:03:04 +0100

    funkcia pat doublou a funkcie druhy double

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -6,12 +6,13 @@ namespace Open_Lab_02._01
     {
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+            double[] zoznam = { 1.11, 2.22, 3.33, 4.44, 5.55 };
+            return zoznam;
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers[1];
         }
     }
 }
